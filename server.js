200052e78b7dfe3d44261c8b2e366b55bb25a14d
215138848 <80263159+215138848@users.noreply.github.com>
2021-09-10 14:01:20 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-type', 'text/html');
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        } else{
+            res.end(data);
+        }
+    });
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requesting on port 3000')
+});
+
